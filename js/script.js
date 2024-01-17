db0b617d51f565f6560f6ed96b2781ff4c8bbd91
rakaghataf <rakaghataf2406@gmail.com>
2024-01-17 17:44:04 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -5,19 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     hitungButton.addEventListener('click', function () {
         var sisi = parseFloat(squareSideInput.value);
-
         if (isNaN(sisi) || sisi <= 0) {
             alert('Masukkan nilai sisi yang valid.');
             return;
         }
-
+        
         var luas = sisi * sisi;
         var keliling = 4 * sisi;
-
         var resultContainer = document.getElementById('resultContainer');
         resultContainer.innerHTML = 'Luas Persegi: ' + luas + '<br>Keliling Persegi: ' + keliling;
     });
-
+    
     resetButton.addEventListener('click', function () {
         squareSideInput.value = '';
         resultContainer.innerHTML = '';
